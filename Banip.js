7a494bbfc0526a45929d388fa451e2d4391b70c4
sombrascript <123718715+sombrascript@users.noreply.github.com>
2023-01-27 04:05:37 -0300

    Update Banip.js

--- Banip.js
+++ Banip.js
@@ -1,3 +1,3 @@
-    var banip="aa 231.91.96 gggggggggggggg122.231.91.96 190.211.91.54 dasdasdsa 190.211.91.96  qwe 12211.91.hhhhhhhh";
+    var banip="aaa 231.91.96 gggggggggggggg122.231.91.96 190.211.91.54 dasdasdsa 190.211.91.96  qwe 12211.91.hhhhhhhh";
  
 	
