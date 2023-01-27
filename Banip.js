0fb303adc55b6283cf48768f9a5aab54fe55b27f
sombrascript <123718715+sombrascript@users.noreply.github.com>
2023-01-27 04:11:31 -0300

    Update Banip.js

--- Banip.js
+++ Banip.js
@@ -1,3 +1,3 @@
-    var banip=" testetr 122.231.91.92 190.211.91.54 333333333 dasdasdsaqwe 1922211.91.96";
+    var banip=" testet 122.231.91.92 190.211.91.54 333333333 dasdasdsaqwe 1922211.91.96";
  
 	
